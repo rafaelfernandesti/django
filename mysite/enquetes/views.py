ae85532ab5d2c8977509339089fa445d4bbc7179
Rafael Fernandes <rafa.fernan10@gmail.com>
2023-05-07 19:55:07 -0300

    Adicionando views

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello world! Você está no índice da enquete")
 # Create your views here.
+def detail(request, question_id):
+    return HttpResponse("Você está olhando para a questão: %s." % question_id)
+def results(request, question_id):
+    response = "Você está olhando para os resultados da questão %s."
+    return HttpResponse(response % question_id)
+def vote(request, question_id):
+    return HttpResponse("Você está votando na questão %s." % question_id)
