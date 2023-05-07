c99d154dcd5fcaf5302976637cdf9f8426c8ac11
Rafael Fernandes <rafa.fernan10@gmail.com>
2023-05-07 20:15:25 -0300

    Aplicando template

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello world! Você está no índice da enquete")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("enquetes/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 # Create your views here.
+
+
 def detail(request, question_id):
     return HttpResponse("Você está olhando para a questão: %s." % question_id)
+
+
 def results(request, question_id):
     response = "Você está olhando para os resultados da questão %s."
     return HttpResponse(response % question_id)
+
+
 def vote(request, question_id):
     return HttpResponse("Você está votando na questão %s." % question_id)
