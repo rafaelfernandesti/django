ba851b7c0a019cda572b8358ee2ff6f4dd67205e
Rafael Fernandes <rafa.fernan10@gmail.com>
2023-05-07 20:18:22 -0300

    Refatorando com render

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Question
 
+
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("enquetes/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "enquetes/index.html", context)
 # Create your views here.
 
 
